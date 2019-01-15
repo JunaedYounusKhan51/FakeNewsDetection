8fe165035a5c8cfea2a4108c3bfb00e941fce779
JunaedYounusKhan51 <junaed292@gmail.com>
2019-01-15 15:16:54 +0600

    Add files via upload

---
+++ Codes/Traditional_ML_based_Methods/8_KNN__Empath/2_fake_real__empath/empath_test.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import numpy as np
+from sklearn import svm
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score
+import pickle
+from sklearn.tree import tree
+
+
+# Read in muffin and cupcake ingredient data# Read i
+#recipes = pd.read_csv('test_liar_feature.csv')
+recipes = pd.read_csv('Empath_fake_or_real_news_test_feature.csv')
+#recipes
+
+print("csv load done")
+
+X = np.array(recipes.drop(['label'], 1))
+y = np.array(recipes['label'])
+
+
+
+print("label load done")
+# Feature names
+
+
+
+##########################
+filename = 'lr.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+#result = loaded_model.score(X, y)
+#print(result)
+
+
+
+pred = loaded_model.predict(X)
+
+print("###################")
+print(".")
+print("test results: ")
+print("---------lr---------------------")
+print ("test_accuracy: ")
+print (accuracy_score(y, pred))
+
+print ("test_precision: ")
+print (precision_score(y, pred, average="weighted"))
+
+print ("test_recall: ")
+print (recall_score(y, pred, average="weighted"))
+
+print ("test_f1 ")
+print (f1_score(y, pred, average="weighted"))
+
+
+filename = 'lr_pickle.pickle'
+pickle.dump((y,pred), open(filename, 'wb'))
+
+
+
+
+#####################################
+
+filename = 'tree.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+#result = loaded_model.score(X, y)
+#print(result)
+
+
+
+pred = loaded_model.predict(X)
+
+print("###################")
+print(".")
+print("test results: ")
+print("------------tree------------------")
+print ("test_accuracy: ")
+print (accuracy_score(y, pred))
+
+print ("test_precision: ")
+print (precision_score(y, pred, average="weighted"))
+
+print ("test_recall: ")
+print (recall_score(y, pred, average="weighted"))
+
+print ("test_f1 ")
+print (f1_score(y, pred, average="weighted"))
+
+
+filename = 'tree_pickle.pickle'
+pickle.dump((y,pred), open(filename, 'wb'))
+
+
+
+######################################
+
+filename = 'knn.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+#result = loaded_model.score(X, y)
+#print(result)
+
+
+
+pred = loaded_model.predict(X)
+
+print("###################")
+print(".")
+print("test results: ")
+print("-----------knn-------------------")
+print ("test_accuracy: ")
+print (accuracy_score(y, pred))
+
+print ("test_precision: ")
+print (precision_score(y, pred, average="weighted"))
+
+print ("test_recall: ")
+print (recall_score(y, pred, average="weighted"))
+
+print ("test_f1 ")
+print (f1_score(y, pred, average="weighted"))
+
+
+filename = 'knn_pickle.pickle'
+pickle.dump((y,pred), open(filename, 'wb'))
+
+
+###########################################
+
+
+filename = 'nb.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+#result = loaded_model.score(X, y)
+#print(result)
+
+
+
+pred = loaded_model.predict(X)
+
+print("###################")
+print(".")
+print("test results: ")
+print("---------nb---------------------")
+print ("test_accuracy: ")
+print (accuracy_score(y, pred))
+
+print ("test_precision: ")
+print (precision_score(y, pred, average="weighted"))
+
+print ("test_recall: ")
+print (recall_score(y, pred, average="weighted"))
+
+print ("test_f1 ")
+print (f1_score(y, pred, average="weighted"))
+
+
+filename = 'nb_pickle.pickle'
+pickle.dump((y,pred), open(filename, 'wb'))
+###################################
+
+###
+
+
+
+
+
+
